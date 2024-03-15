1844afdcb42a8f8e96aca1cf54da789de09f1d12
Andreas Wolf <dev@a-w.io>
2024-03-15 18:20:41 +0100

    [TASK] Add CI pipeline

---
+++ .build/ecs.php
@@ -0,0 +1,19 @@
+<?php
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withRules([
+        NoUnusedImportsFixer::class,
+        ArraySyntaxFixer::class,
+        DeclareStrictTypesFixer::class,
+    ])
+    ->withPreparedSets(psr12: true)
+    ->withPaths([
+        __DIR__,
+        __DIR__ . '/../src/',
+        __DIR__ . '/../tests/',
+    ]);
